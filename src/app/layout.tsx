9abb8522cda42658f317c3f97ee049869a5313e3
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2023-12-17 18:04:44 +0900

    feat: 폰트 설정 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from 'next'
 import '../style/globals.css';
 import StyledComponentsRegistry from '@/lib/styled-components-registry'
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal'],
+  variable: '--font-roboto',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: 'W Calendar',
@@ -13,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={roboto.className}>
       <body>
         <StyledComponentsRegistry>
           {children}
