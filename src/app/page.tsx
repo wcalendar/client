f4a420014dd83eded70d787f5e5b6d31af40a5c1
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2023-12-20 18:57:20 +0900

    fix: page.tsx 수정 복구

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 'use client';
 
 import Header from '@/components/common/Header';
-import FixedModal from '@/components/common/fixed-modal/FixedModal';
-import { useState } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.main`
   width: 100%;
   height: 100vh;
-  position: relative;
 `;
 
 const Main = styled.div`
@@ -28,33 +25,12 @@ const CalendarContainer = styled.div`
   padding: 1rem;
 `;
 
-const InModal = styled.div`
-  width: 100%;
-  height: 300px;
-  padding: 16px;
-  background-color: red;
-`;
-
 export default function Home() {
-  const [isModalOpen, setModalOpen] = useState(false);
-
-  const handleOpenModal = () => {
-    setModalOpen(true);
-  }
-  const handleCloseModal = () => {
-    setModalOpen(false);
-  }
-
   return (
     <Container>
-      {isModalOpen && (
-        <FixedModal width='400px' title='일정 등록' buttonList={[{ text: '저장', onClick: () => {} }, { text: '취소', onClick: handleCloseModal }]} onClose={handleCloseModal} >
-          <InModal />
-        </FixedModal>
-      )}
       <Header />
       <Main>
-        <CategoryContainer onClick={handleOpenModal}>Category</CategoryContainer>
+        <CategoryContainer>Category</CategoryContainer>
         <CalendarContainer>Calendar</CalendarContainer>
       </Main>
     </Container>
