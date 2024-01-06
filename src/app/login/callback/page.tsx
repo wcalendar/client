a5373cf681dfb8265a5a80a1aec39abd21b45450
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-01-06 19:24:16 +0900

    feat: 로그인 후 토큰 로컬스토리지에 저장

--- src/app/login/callback/page.tsx
+++ src/app/login/callback/page.tsx
@@ -1,25 +1,38 @@
 'use client';
 
 import { useEffect } from "react";
-import axios from "axios";
-import { useRouter } from "next/navigation";
+import axios, { AxiosHeaders } from "axios";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function Callback() {
   const router = useRouter();
+  const state = useSearchParams().get('state');
 
   const callback = async () => {
-    // const response = await axios.get('https://wplanner.co.kr/api/token/access', {
-    //   withCredentials: true,
-    // });
+    const response = await axios.get(`https://wplanner.co.kr/api/token/access/${state}`, {
+      withCredentials: true,
+    });
 
-    // if(response.status < 300) {
-    //   console.log(response);
-    // }
+    if(response.status < 300) {
+      const authorization: string | undefined = response.headers['authorization']
+      if(authorization) {
+        const token = authorization.split(' ')[1];
+        localStorage.setItem('at', token);
+      } else {
+        alert('로그인 실패');
+        router.push('/login');
+      }
+    }
 
-    // router.push('/');
+    router.push('/');
   }
 
   useEffect(() => {
+    if(!state) {
+      alert('로그인 실패');
+      router.push('/login');
+    }
+
     callback();
   }, []);
 
