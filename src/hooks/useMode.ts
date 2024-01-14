66f205c8e9809e3f0d94b9efbffc034319b9774a
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-01-15 00:18:42 +0900

    feat: useMode 훅 추가

---
+++ src/hooks/useMode.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export default function useMode(key: string) {
+  const [isMode, setMode] = useState(false);
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if(e.key === key) {
+        setMode(true);
+      }
+    };
+
+    const handleKeyUp = (e: KeyboardEvent) => {
+      if(e.key === key) {
+        setMode(false);
+      }
+    }
+
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('keyup', handleKeyUp);
+    };
+  }, []);
+
+  return [ isMode ];
+}
