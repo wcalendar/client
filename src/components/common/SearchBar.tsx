4f7db3ee50c729ecc988aea1a151a17b0b190a57
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-01-17 14:40:27 +0900

    feat: SearchBar 디자인 수정

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -1,18 +1,40 @@
+import { mdiMagnify } from '@mdi/js';
+import Icon from '@mdi/react';
 import { RiSearchLine } from 'react-icons/ri';
 import styled from 'styled-components';
 
-const SearchBarContainer = styled.form`
-  display: flex;
-  align-items: center;
-  gap: 1rem;
+const Container = styled.form`
+  width: 25rem;
+  height: 1.875rem;
+  border: 1px solid ${({ theme }) => theme.colors.gray};
+  border-radius: 10px;
+  cursor: text;
+  position: relative;
 `;
+
+const IconWrapper = styled.div`
+  width: 1.5rem;
+  height: 1.5rem;
+  position: absolute;
+  right: .5rem;
+  top: calc(50% - .75rem);
+  color: ${({ theme }) => theme.colors.gray};
+
+  svg {
+    color: inherit;
+  }
+
+  path {
+    color: inherit;
+  }
+`;
+
 export default function SearchBar() {
   return (
-    <SearchBarContainer>
-      <input />
-      <button>
-        <RiSearchLine size={24} />
-      </button>
-    </SearchBarContainer>
+    <Container>
+      <IconWrapper>
+        <Icon path={mdiMagnify} />
+      </IconWrapper>
+    </Container>
   );
 }
