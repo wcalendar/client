69da0782e589a6e912792bd454d0c9c9ec332130
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-01-20 10:27:14 +0900

    refactor: AppModal 코드 수정

--- src/components/common/app-modal/AppModal.tsx
+++ src/components/common/app-modal/AppModal.tsx
@@ -23,17 +23,23 @@ export default function AppModal() {
         const { key, modalProps } = modalKey;
 
         if(key === 'schedule') {
-          const Comp = modalMap[key];
-          return <Comp key={'modal'} {...modalProps}></Comp>
+          // TODO 변수명 수정
+          const ScheduleModals = modalMap[key];
+          // TODO key 수정
+          return <ScheduleModals key={'modal'} {...modalProps}></ScheduleModals>
+
         } else if(key === 'category') {
-          const Comp = modalMap[key];
-          return <Comp key={'modal'} {...modalProps}></Comp>
+          const CategoryModals = modalMap[key];
+          return <CategoryModals key={'modal'} {...modalProps}></CategoryModals>
+
         } else if(key === 'newSchedule') {
-          const Comp = modalMap[key];
-          return <Comp key={'modal'} {...modalProps}></Comp>
+          const NewScheduleModals = modalMap[key];
+          return <NewScheduleModals key={'modal'} {...modalProps}></NewScheduleModals>
+
         } else if(key === 'search') {
-          const Comp = modalMap[key];
-          return <Comp key={'modal'} {...modalProps}></Comp>
+          const SearchModals = modalMap[key];
+          return <SearchModals key={'modal'} {...modalProps}></SearchModals>
+          
         }
       })}
     </>
