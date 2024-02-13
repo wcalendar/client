44b4cb57cea0ae12ad581ed91dfce00903d5b50f
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-02-13 12:58:39 +0900

    chore: 카테고리 관리 페이지 currentDate 적용

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -1,22 +1,16 @@
 'use client';
 
 import CategoryHeader from '@/app/category/CategoryHeader';
-import time from '@/lib/time';
-import { Dayjs } from 'dayjs';
-import { useState } from 'react';
 import styled from 'styled-components';
 import CategoryBody from './CategoryBody';
+import { useCurrentDate } from '@/providers/CurrentDateProvider/useCurrentDate';
 
 const Container = styled.main`
   position: relative;
 `;
 
 export default function CategoryPage() {
-  const [currentDate, setDate] = useState<Dayjs>(time.now());
-
-  const handleDateChange = (value: Dayjs) => {
-    setDate(value);
-  };
+  const { currentDate } = useCurrentDate();
 
   return (
     <Container>
