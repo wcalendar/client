a52fe8fb40c5d44269c842ff8b627b9e6d23103d
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-02-14 12:34:48 +0900

    chore: NewScheduleModal 삭제 기능 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -396,7 +396,7 @@ export default function Home() {
   }, [handleScheduleCreate]);
 
   const handleUpdateScheduleClick = useCallback((schedule: ScheduleToRender) => {
-    openNewScheduleModal({ schedule });
+    openNewScheduleModal({ updateScheduleInfo: { schedule, onScheduleDelete: handleScheduleDelete } });
   }, [openNewScheduleModal]);
 
   const handleOpenNewScheduleModal = () => {
