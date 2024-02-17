66b6df49b3b8cf0049b37a73445806e2a49f0d8e
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-02-17 23:57:45 +0900

    chore: SWR 로 fetch하는 API 예외 처리

--- src/lib/SWRConfigProvider.tsx
+++ src/lib/SWRConfigProvider.tsx
@@ -20,17 +20,34 @@ export default function SWRConfigProvider({
     <SWRConfig
       value={{
         shouldRetryOnError: false,
-        onError: (error: AxiosError, key) => {
+        onError: (error: AxiosError<any>, key) => {
           if(error.response) {
-            openPopup({
-              title: '오류',
-              description: <>로그인이 필요한 서비스입니다</>,
-              buttons: [{ label: '확인', onClick: () => { closePopup(); router.push('/login'); } }],
-            });
+            if(error.response.status === 401) {
+              openPopup({
+                title: '오류 발생',
+                description: <>로그인이 필요한 서비스입니다</>,
+                buttons: [{ label: '확인', onClick: () => { closePopup(); router.push('/login'); } }],
+              });
+            } else {
+              if(error.response.data.errorCode) {
+                const errorCode = error.response.data.errorCode;
+                openPopup({
+                  title: '오류',
+                  description: <>{errorCode} - 확인되지 않은 오류입니다<br />문의하기를 통해 문의해주세요</>,
+                  buttons: [{ label: '확인', onClick: closePopup }],
+                });
+              } else {
+                openPopup({
+                  title: '오류',
+                  description: <>{error.response.status} - 확인되지 않은 오류입니다<br />문의하기를 통해 문의해주세요</>,
+                  buttons: [{ label: '확인', onClick: closePopup }],
+                });
+              }
+            }
           } else {
             openPopup({
               title: '오류',
-              description: <>심각한 오류가 발생했습니다</>,
+              description: <>unknown - 확인되지 않은 오류입니다<br />문의하기를 통해 문의해주세요</>,
               buttons: [{ label: '확인', onClick: closePopup }],
             });
           }
