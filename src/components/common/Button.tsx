5f1333ad35ebc056eea578f3fc0dd9ae3c12a71d
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-02-22 22:23:07 +0900

    feat: Button 컴포넌트 구현

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,67 @@
+import svgs, { SVGKey } from "@/assets/svgs";
+import React, { ReactNode, SVGProps } from "react";
+import styled from "styled-components";
+
+const Container = styled.button.withConfig({
+  shouldForwardProp: p => !['width', 'size', 'type', 'disabled'].includes(p),
+})<{ width: string, size: ButtonSize, type: ButtonType, disabled: boolean }>`
+  --color: ${({ type, theme, disabled }) => type === 'primary' ? theme.colors.white : (disabled ? `${theme.colors.black}20` : `${theme.colors.black}cc`)};
+
+  display: flex;
+  justify-content: center;
+  gap: .5rem;
+  align-items: center;
+  width: ${({ width }) => width};
+  height: ${({ size }) => size === 'small' ? '2.5rem' : (size === 'medium' ? '3.75rem' : '4.5rem')};
+  background-color: ${({ type, theme, disabled }) => type === 'primary' ? `${theme.colors.primary}${disabled ? '33' : ''}` : (disabled ? `${theme.colors.black}0d` : theme.colors.white)};
+  color: var(--color);
+  border: ${({ type, theme, disabled }) => type === 'primary' || disabled ? 'none' : `1px solid ${theme.colors.black}33`};
+  border-radius: 8px;
+  font-size: ${({ size }) => size === 'small' ? '.9375rem' : (size === 'medium' ? '1.25rem' : '1.375rem')};
+  transition: .25s background-color ease;
+  cursor: pointer;
+
+  ${({ disabled, type, theme }) => disabled ? '' : `
+  &:hover {
+    background-color: ${type === 'primary' ? theme.colors.primaryHover : theme.colors.whiteHover};
+  }
+  `}
+
+  svg {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+
+  path {
+    fill: var(--color);
+  }
+`;
+
+type ButtonSize = 'big' | 'medium' | 'small';
+type ButtonType = 'primary' | 'white';
+
+interface ButtonProps {
+  width?: string;
+  size?: ButtonSize;
+  type?: ButtonType;
+  icon?: SVGKey;
+  disabled?: boolean;
+  children: ReactNode;
+}
+
+export default function Button({
+  width = '100%',
+  size = 'small',
+  type = 'primary',
+  icon,
+  disabled,
+  children,
+}: ButtonProps) {
+  const Icon = icon ? svgs[icon] : undefined;
+  return (
+    <Container width={width} size={size} type={type} disabled={Boolean(disabled)}>
+      {Icon && <Icon color="" />}
+      {children}
+    </Container>
+  );
+}
