004b214bd218cdd9b62d56368768f82532c4e7e0
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-02-22 21:30:25 +0900

    fix: StoryBook SVG 파일 보여주지 않던 오류 수정
    * Storybook Webpack 설정 수정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,28 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  webpackFinal: async config => {
-    config.module?.rules?.unshift({
-      test: /\.svg$/,
-      use: ['@svgr/webpack',]
-    });
+  webpackFinal: async (config) => {
+    if (!config.module || !config.module.rules) {
+      return config;
+    }
 
+    config.module.rules = [
+      ...config.module.rules.map((rule) => {
+        if (!rule || rule === '...') {
+          return rule;
+        }
+
+        if (rule.test && /svg/.test(String(rule.test))) {
+          return { ...rule, exclude: /\.svg$/i };
+        }
+        return rule;
+      }),
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack']
+      }
+    ]
+    
     return config;
   },
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
