44069ede1ae26ddd1e9ee59299abf6cdfae55014
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-02-22 21:22:17 +0900

    chore: StoryBook 에서 svg 파일을 사용하기 위한 webpack 설정, svg 컴포넌트 타입 지정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,14 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
+  webpackFinal: async config => {
+    config.module?.rules?.unshift({
+      test: /\.svg$/,
+      use: ['@svgr/webpack',]
+    });
+
+    return config;
+  },
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
