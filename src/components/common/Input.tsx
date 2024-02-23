f929483d1bc67821f342f7f155f3955a27b5cb88
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-02-23 20:50:12 +0900

    feat: Input 컴포넌트 구현

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,86 @@
+import styled from 'styled-components';
+import { useCallback, ChangeEventHandler } from 'react';
+
+const Container = styled.div.withConfig({
+  shouldForwardProp: p => !['width'].includes(p),
+})<{ width: string }>`
+  position: relative;
+  width: ${({ width }) => width};
+`;
+
+const TextInput = styled.textarea`
+  width: 100%;
+  min-height: 40px;
+  padding: .5rem;
+  border: 1px solid ${({ theme }) => `${theme.colors.black}33`};
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.colors.white};
+  font-size: .9375rem;
+  line-height: 1.375rem;
+  color: ${({ theme }) => `${theme.colors.black}cc`};
+  resize: none;
+  font-family: none;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:disabled {
+    background-color: ${({ theme }) => `${theme.colors.black}0d`};
+  }
+`;
+
+const Description = styled.div`
+  margin-top: .25rem;
+  font-size: .6875rem;
+  color: ${({ theme }) => `${theme.colors.black}80`};
+  user-select: none;
+`;
+
+interface InputProps {
+  width?: string;
+  placeholder?: string;
+  value?: string;
+  onChange?: (value: string) => void;
+  disabled?: boolean;
+  description?: string;
+}
+
+export default function Input({
+  width = '100%',
+  placeholder,
+  value,
+  onChange,
+  disabled,
+  description,
+}: InputProps) {
+  const handleChange: ChangeEventHandler<HTMLTextAreaElement> = useCallback((e) => {
+    const textarea = (e.target as HTMLTextAreaElement);
+    // 개행 문자 입력 금지
+    const newValue = textarea.value.replace(/(\r\n|\n|\r)/gm, '');
+
+    if(onChange) {
+      onChange(newValue);
+    }
+    
+    // value 가 props 에 포함되어 있지 않을 때는 직접 newValue로 변경
+    textarea.value = newValue;
+    textarea.style.height = 'auto';
+    textarea.style.height = (textarea.scrollHeight + 2) + 'px';
+  }, [onChange]);
+
+  return (
+    <Container width={width} >
+      {value ? (
+        <TextInput value={value} placeholder={placeholder} rows={1} wrap='on' onChange={handleChange} disabled={disabled} />
+      ) : (
+        <TextInput placeholder={placeholder} rows={1} wrap='on' onChange={handleChange} disabled={disabled} />
+      ) }
+      {description && (
+        <Description>
+          *{description}
+        </Description>
+      )}
+    </Container>
+  )
+}
