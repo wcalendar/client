a5de0dbf62ea3bdc7aaf4c3f476d30df7d984123
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-03-02 19:41:03 +0900

    feat: SearchBar 구현

---
+++ src/components/common/SearchBar/SearchBar.tsx
@@ -0,0 +1,126 @@
+import Svgs from "@/assets/Svgs";
+import { newSearchDummyData } from "@/dummies/calendar";
+import styled from "styled-components"
+import SearchResultItem from "./SearchResultItem";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { ModalStatus } from "@/types";
+
+const Container = styled.div`
+  position: relative;
+  width: 21.25rem;
+  height: 2.5rem;
+  overflow: visible;
+`;
+
+const SearchInput = styled.input`
+  width: 100%;
+  height: 100%;
+  font-size: .9375rem;
+  padding-left: .75rem;
+  padding-right: 2.75rem;
+  border-radius: .5rem;
+  border: 1px solid ${({ theme }) => `${theme.colors.black}33`};
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const SearchIconWrapper = styled.div`
+  position: absolute;
+  top: .625rem;
+  right: .75rem;
+  width: 1.25rem;
+  height: 1.25rem;
+
+  path {
+    fill: ${({ theme }) => `${theme.colors.black}33`};
+  }
+`;
+
+const SearchListWrapper = styled.div.withConfig({
+  shouldForwardProp: p => !['status'].includes(p),
+})<{ status: ModalStatus }>`
+  position: absolute;
+  top: 3rem;
+  z-index: 10;
+  width: 100%;
+  height: 16.25rem;
+  border-radius: .5rem;
+  border: 1px solid ${({ theme }) => `${theme.colors.black}33`};
+  box-shadow: 4px 4px 12px 0 ${({ theme }) => `${theme.colors.black}1F`};
+  padding: .75rem;
+  overflow-y: auto;
+  transform: translateY(${({ status }) => status === 'open' ? '0%' : '-10%'});
+  opacity: ${({ status }) => status === 'open' ? 1 : 0};
+  animation: ${({ status }) => status === 'open' ? 'fromUpOpen' : 'fromUpClose'} .25s;
+  background-color: ${({ theme }) => theme.colors.white};
+`;
+
+const NoResult = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  font-size: .9375rem;
+  color: ${({ theme }) => `${theme.colors.black}80`};
+  user-select: none;
+`;
+
+const SearchList = styled.div`
+  height: auto;
+`;
+
+export default function SearchBar() {
+  const searchInputRef = useRef<HTMLInputElement>(null);
+  const searchListRef = useRef<HTMLDivElement>(null);
+
+  const [status, setStatus] = useState<ModalStatus>('closed');
+
+  const list = newSearchDummyData;
+
+  useEffect(() => {
+    const handleOutsideClick = (e: MouseEvent) => {
+      if(searchInputRef.current && !searchInputRef.current.contains(e.target as Node) && searchListRef.current && !searchListRef.current.contains(e.target as Node)) {
+        handleListClose();
+      }
+    }
+
+    document.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    }
+  }, []);
+
+  const handleInputFocus = useCallback(() => {
+    setStatus('open');
+  }, []);
+
+  const handleListClose = useCallback(() => {
+    setStatus('closing');
+  }, []);
+
+  const handleListAnimationEnd = useCallback(() => {
+    if(status === 'closing') setStatus('closed');
+  }, [status]);
+
+  return (
+    <Container>
+      <SearchInput placeholder='일정 검색' onClick={handleInputFocus} ref={searchInputRef} />
+      <SearchIconWrapper>
+        <Svgs svgKey="search" />
+      </SearchIconWrapper>
+      {status !== 'closed' && (
+        <SearchListWrapper status={status} ref={searchListRef} onAnimationEnd={handleListAnimationEnd}>
+          {/* <NoResult>해당하는 일정이 없습니다.</NoResult> */}
+          <SearchList>
+            {list.map((item, i) => (
+              <SearchResultItem key={`search-result-${item.scheduleGroupCode}-${i}`} searchResult={item} ></SearchResultItem>
+            ))}
+          </SearchList>
+        </SearchListWrapper>
+      )}
+    </Container>
+  );
+}
---
+++ src/components/common/SearchBar/SearchResultItem.tsx
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+import Chip from "../Chip";
+import { CategoryColor, NewSearchedScheduleDto } from "@/types";
+
+const Container = styled.div`
+  height: auto;
+  padding: .5rem;
+  cursor: pointer;
+  transition: background-color .25s ease;
+
+  &:hover {
+    border-radius: .5rem;
+    background-color: ${({ theme }) => `${theme.colors.black}0D`};
+  }
+
+  &+& {
+    border-top: 1px solid ${({ theme }) => `${theme.colors.black}1A`};
+  }
+`;
+
+const Category = styled.div`
+  display: flex;
+  align-items: center;
+  gap: .25rem;
+  font-size: .6875rem;
+  font-weight: bold;
+  color: ${({ theme }) => `${theme.colors.black}33`};
+  user-select: none;
+  margin-bottom: .5rem;
+`;
+
+const Content = styled.div`
+  font-size: .9375rem;
+  font-weight: bold;
+  user-select: none;
+  margin-bottom: .25rem;
+`;
+
+const MemoAlarm = styled.span.withConfig({
+  shouldForwardProp: p => !['color'].includes(p),
+})<{ color: CategoryColor}>`
+  display: inline-block;
+  width: .25rem;
+  height: .25rem;
+  border-radius: .25rem;
+  background-color: ${({ color, theme }) => theme.colors.category(color, 0) };
+  margin-left: .25rem;
+  vertical-align: top;ㅐㅍ
+`;
+
+const Date = styled.div`
+  font-size: .6875rem;
+  user-select: none;
+  color: ${({ theme }) => `${theme.colors.black}80`};
+`;
+
+interface SearchResultItemProps {
+  searchResult: NewSearchedScheduleDto;
+}
+
+export default function SearchResultItem({
+  searchResult,
+}: SearchResultItemProps) {
+  const { categoryNames, categoryColor, scheduleContent, scheduleStartDate, scheduleEndDate, scheduleMemo, } = searchResult;
+
+  return (
+    <Container>
+      <Category>
+        <Chip width='3.75rem' label={categoryNames[0]} color='newRed' />
+        {categoryNames[1] && <>/<Chip width='3.75rem' label={categoryNames[1]} color='newRed' /></>}
+        {categoryNames[2] && <>/<Chip width='3.75rem' label={categoryNames[2]} color='newRed' /></>}
+      </Category>
+      <Content>
+        {scheduleContent}
+        {scheduleMemo && <MemoAlarm color={categoryColor} />}
+      </Content>
+      <Date>
+        {scheduleStartDate} ~ {scheduleEndDate}
+      </Date>
+    </Container>
+  );
+}
