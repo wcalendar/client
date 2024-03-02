f28b63c64284649dac63dec92460ea198b4a7406
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-03-02 18:35:08 +0900

    chore: Search Svg 추가

--- src/assets/Svgs.tsx
+++ src/assets/Svgs.tsx
@@ -5,13 +5,14 @@ import LogoSmallIcon from '@/assets/logo_small.svg';
 import MyPageIcon from '@/assets/my_page.svg';
 import UnionIcon from '@/assets/union.svg';
 import PreferenceIcon from '@/assets/preference.svg';
+import SearchIcon from '@/assets/search.svg';
 import ArrowLeftIcon from '@/assets/arrow_left.svg';
 import SmallCheckBoxOpenIcon from '@/assets/check_box_small_open.svg';
 import SmallCheckBoxClosedIcon from '@/assets/check_box_small_closed.svg';
 import BigCheckBoxOpenIcon from '@/assets/check_box_big_open.svg';
 import BigCheckBoxClosedIcon from '@/assets/check_box_big_closed.svg';
 
-export type SVGKey = 'myPage' | 'union' | 'preference' | 'smallCheckBoxOpen' | 'smallCheckBoxClosed' | 'bigCheckBoxOpen' | 'bigCheckBoxClosed' | 'arrowLeft' | 'logoBig' | 'logoSmall' | 'mainLogoBig' | 'mainLogoSmall';
+export type SVGKey = 'myPage' | 'union' | 'preference' | 'search' | 'smallCheckBoxOpen' | 'smallCheckBoxClosed' | 'bigCheckBoxOpen' | 'bigCheckBoxClosed' | 'arrowLeft' | 'logoBig' | 'logoSmall' | 'mainLogoBig' | 'mainLogoSmall';
 
 interface SvgsProps {
   svgKey: SVGKey;
@@ -27,6 +28,7 @@ export default function Svgs({
   if(svgKey === 'myPage') return <MyPageIcon />;
   if(svgKey === 'union') return <UnionIcon />;
   if(svgKey === 'preference') return <PreferenceIcon />;
+  if(svgKey === 'search') return <SearchIcon />;
   if(svgKey === 'arrowLeft') return <ArrowLeftIcon />;
   if(svgKey === 'bigCheckBoxOpen') return <BigCheckBoxOpenIcon />;
   if(svgKey === 'bigCheckBoxClosed') return <BigCheckBoxClosedIcon />;
