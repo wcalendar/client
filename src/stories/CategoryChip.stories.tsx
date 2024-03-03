ce9bb7b863f799fdf2b1f0515a27a61075114417
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-03-03 17:36:42 +0900

    chore: CategoryChip 스토리 추가

---
+++ src/stories/CategoryChip.stories.tsx
@@ -0,0 +1,65 @@
+import CategoryChip from "@/components/calendar/CategoryChip";
+import time from "@/lib/time";
+import { NewCategoryToRender } from "@/types";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: 'Components/Calendar/CategoryChip',
+  component: CategoryChip,
+} satisfies Meta<typeof CategoryChip>
+
+export default meta;
+type Story = StoryObj<typeof meta>
+
+const categoryToRender: NewCategoryToRender = {
+  category: {
+    id: 'c1',
+    name: 'Category 1',
+    level: 0,
+    color: 'newRed',
+    startDate: time.new(2024, 2, 1),
+    endDate: time.new(2030, 2, 1),
+    description: 'This is Category 1',
+    isVisible: false,
+    parentId: null,
+    schedules: [],
+    children: [{
+      category: {
+        id: 'c2',
+        name: 'Category 2',
+        level: 1,
+        color: 'newRed',
+        startDate: time.new(2024, 2, 1),
+        endDate: time.new(2030, 2, 1),
+        description: 'This is Category 2',
+        isVisible: false,
+        parentId: 'c1',
+        schedules: [],
+        children: [{
+          category: {
+            id: 'c3',
+            name: 'Category 3',
+            level: 2,
+            color: 'newRed',
+            startDate: time.new(2024, 2, 1),
+            endDate: time.new(2030, 2, 1),
+            description: 'This is Category 3',
+            isVisible: false,
+            parentId: 'c2',
+            schedules: [],
+            children: [],
+          },
+          lines: [],
+        }],
+      },
+      lines: [],
+    }],
+  },
+  lines: [],
+};
+
+export const Default: Story = {
+  args: {
+    categoryToRender,
+  }
+}
