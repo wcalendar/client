316e16cbc76b0829d1401f62afc625db281445a9
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-03-04 16:36:02 +0900

    chore: Example Story 추가

--- src/stories/CategoryChip.stories.tsx
+++ src/stories/CategoryChip.stories.tsx
@@ -11,7 +11,7 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>
 
-const categoryToRender: NewCategoryToRender = {
+const defaultCategoryToRender: NewCategoryToRender = {
   category: {
     id: 'c1',
     name: 'Category 1',
@@ -58,8 +58,78 @@ const categoryToRender: NewCategoryToRender = {
   lines: [],
 };
 
+const exampleCategoryToRender: NewCategoryToRender = {
+  category: {
+    id: 'c1',
+    name: 'Category 1',
+    level: 0,
+    color: 'newRed',
+    startDate: time.new(2024, 2, 1),
+    endDate: time.new(2030, 2, 1),
+    description: 'This is Category 1',
+    isVisible: false,
+    parentId: null,
+    schedules: [],
+    children: [{
+      category: {
+        id: 'c2',
+        name: 'Category 2',
+        level: 1,
+        color: 'newRed',
+        startDate: time.new(2024, 2, 1),
+        endDate: time.new(2030, 2, 1),
+        description: 'This is Category 2',
+        isVisible: false,
+        parentId: 'c1',
+        schedules: [],
+        children: [{
+          category: {
+            id: 'c3',
+            name: 'Category 3',
+            level: 2,
+            color: 'newRed',
+            startDate: time.new(2024, 2, 1),
+            endDate: time.new(2030, 2, 1),
+            description: 'This is Category 3',
+            isVisible: false,
+            parentId: 'c2',
+            schedules: [],
+            children: [],
+          },
+          lines: [],
+        }],
+      },
+      lines: [],
+    },
+    {
+      category: {
+        id: 'c4',
+        name: 'Category 4',
+        level: 1,
+        color: 'newRed',
+        startDate: time.new(2024, 2, 1),
+        endDate: time.new(2030, 2, 1),
+        description: 'This is Category 4',
+        isVisible: false,
+        parentId: 'c1',
+        schedules: [],
+        children: [],
+      },
+      lines: [[], []],
+    }
+  ],
+  },
+  lines: [],
+};
+
 export const Default: Story = {
   args: {
-    categoryToRender,
+    categoryToRender: defaultCategoryToRender,
+  }
+}
+
+export const Example: Story = {
+  args: {
+    categoryToRender: exampleCategoryToRender,
   }
 }
