1b78b8c7347bdd0625c465b16c01954d3a3acdba
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-03-05 15:03:53 +0900

    chore: MonthPicker Story 추가

---
+++ src/stories/MonthPicker.stories.tsx
@@ -0,0 +1,34 @@
+import MonthPicker from "@/components/common/month-picker/MonthPicker";
+import time from "@/lib/time";
+import { Meta, StoryObj } from "@storybook/react";
+import dayjs, { Dayjs } from "dayjs";
+import { useArgs } from '@storybook/preview-api';
+
+const meta = {
+  title: 'Components/MonthPicker',
+  component: MonthPicker,
+  argTypes: {
+    value: {
+      control: 'date'
+    }
+  },
+} satisfies Meta<typeof MonthPicker>
+
+export default meta;
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    buttonType: 'big',
+    value: time.now(),
+  },
+  render: (args) => {
+    const [{value}, updateArgs] = useArgs<{ value: number }>();
+
+    const handleChange = (value: Dayjs) => {
+      updateArgs({value: value.valueOf()});
+    }
+
+    return <MonthPicker {...args} value={dayjs(value)} onChange={handleChange} />
+  },
+}
