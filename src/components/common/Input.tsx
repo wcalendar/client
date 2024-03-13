84d695b99808d132ee47ac12f14d1e3f7483cbc6
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-03-13 14:52:35 +0900

    chore: Input 색, value를 항상 받도록 수정

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -12,36 +12,37 @@ const TextInput = styled.textarea`
   width: 100%;
   min-height: 40px;
   padding: .5rem;
-  border: 1px solid ${({ theme }) => `${theme.colors.black}33`};
+  border: 1px solid ${({ theme }) => theme.colors.black20};
   border-radius: 8px;
   background-color: ${({ theme }) => theme.colors.white};
   font-size: .9375rem;
   line-height: 1.375rem;
-  color: ${({ theme }) => `${theme.colors.black}cc`};
+  color: ${({ theme }) => theme.colors.black};
   resize: none;
   font-family: none;
+  overflow: hidden;
 
   &:focus {
     outline: none;
   }
 
   &:disabled {
-    background-color: ${({ theme }) => `${theme.colors.black}0d`};
+    background-color: ${({ theme }) => theme.colors.black05};
   }
 `;
 
 const Description = styled.div`
   margin-top: .25rem;
   font-size: .6875rem;
-  color: ${({ theme }) => `${theme.colors.black}80`};
+  color: ${({ theme }) => theme.colors.black50};
   user-select: none;
 `;
 
 interface InputProps {
   width?: string;
   placeholder?: string;
-  value?: string;
-  onChange?: (value: string) => void;
+  value: string;
+  onChange: (value: string) => void;
   disabled?: boolean;
   description?: string;
 }
@@ -59,11 +60,9 @@ export default function Input({
     // 개행 문자 입력 금지
     const newValue = textarea.value.replace(/(\r\n|\n|\r)/gm, '');
 
-    if(onChange) {
-      onChange(newValue);
-    }
+    onChange(e.target.value);
     
-    // value 가 props 에 포함되어 있지 않을 때는 직접 newValue로 변경
+    // 자동 높이 조절
     textarea.value = newValue;
     textarea.style.height = 'auto';
     textarea.style.height = (textarea.scrollHeight + 2) + 'px';
@@ -71,11 +70,7 @@ export default function Input({
 
   return (
     <Container width={width} >
-      {value ? (
-        <TextInput value={value} placeholder={placeholder} rows={1} wrap='on' onChange={handleChange} disabled={disabled} />
-      ) : (
-        <TextInput placeholder={placeholder} rows={1} wrap='on' onChange={handleChange} disabled={disabled} />
-      ) }
+      <TextInput value={value} placeholder={placeholder} rows={1} wrap='on' onChange={handleChange} disabled={disabled} />
       {description && (
         <Description>
           *{description}
